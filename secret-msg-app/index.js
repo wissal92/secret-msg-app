f51868cd4e0ef10f0d8640a8c661665f013e82ad
wissal <wissal.ouarsafi@gmail.com>
2020-01-09 13:04:54 -0800

    Add the functionality that shows the secret message

--- secret-msg-app/index.js
+++ secret-msg-app/index.js
@@ -1,6 +1,19 @@
+const {hash} = window.location;
+
+const msg = atob(hash.replace('#', ''));
+
+if(msg){
+    document.querySelector('#msg-form').classList.add('hide');
+    document.querySelector('#msg-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = msg;
+}
+
 document.querySelector('form').addEventListener('submit', e =>{
     e.preventDefault();
-
+    
+    document.querySelector('#msg-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
     
